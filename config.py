ec6e5b6d0489e6fd1ac22455246e1e6adaeb25e3
Ilya Rodin <85010136+eternal-struggler@users.noreply.github.com>
2022-08-20 16:31:37 +0300

    Add files via upload
    Added tokens

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "<YOUR_TOKEN>"
-tg_bot_token = "<YOUR_TOKEN>"
+open_weather_token = "4d7f86104d1733ab75b246b407dc562e"
+tg_bot_token = "5496201960:AAHOv_xCQ0Msv7BcFixeqFUhzWPk9ymLFV0"
